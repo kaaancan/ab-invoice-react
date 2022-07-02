045df0a7436ece726de07788aae59dc7ff8f4052
Kaan Goetuermen <k_goet@outlook.de>
2022-07-02 15:18:18 +0200

    ci/cd-test

---
+++ src/login/Login.tsx
@@ -0,0 +1,10 @@
+import React, {memo} from "react";
+import {Center} from '@mantine/core'
+
+
+import './Login.css';
+
+export const Login = memo(function Login(): React.ReactElement {
+
+    return  <Center style={{width: 400, height: 800}}>All shit centered</Center>
+})
