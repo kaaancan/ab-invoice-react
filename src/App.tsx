9f1951d94a70b6083062b58e4af970b5b40baaa2
Kaan Goetuermen <k_goet@outlook.de>
2022-07-02 17:14:20 +0200

    change-login-and-add-stepper

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Login} from "./login/Login";
+import {InvoiceStepper} from "./invoice/InvoiceStepper";
+import {withAuthenticator} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from "./aws-exports";
+import {Amplify} from "aws-amplify";
+import {Center} from "@mantine/core";
+
+Amplify.configure(awsExports);
+
 
 function App() {
-  return (
-    <div className="App">
-      <Login></Login>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <Center style={{height: 600}}><InvoiceStepper></InvoiceStepper></Center>
+        </div>
+    );
 }
 
-export default App;
+export default withAuthenticator(App);
---
+++ src/invoice/InvoiceStepper.tsx
@@ -0,0 +1,36 @@
+import React, {memo, useState} from "react";
+import {Button, Group, Stepper} from "@mantine/core";
+import {DateRangePicker} from "@mantine/dates";
+import dayjs from "dayjs";
+
+
+export const InvoiceStepper = memo(function InvoiceStepper(): React.ReactElement {
+    const [active, setActive] = useState(0);
+    const nextStep = () => setActive((current) => (current < 2 ? current + 1 : current));
+    const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
+    const [value, setValue] = useState<[Date | null, Date | null]>([
+        dayjs().startOf("month").toDate(),
+        dayjs().endOf("month").toDate(),
+    ]);
+    return (
+        <div>
+            <Stepper active={active} onStepClick={setActive} breakpoint="sm">
+                <Stepper.Step label="İlk adım" description="Ayı seçin">
+                    <DateRangePicker label={"Faturalarin tarihi secin"} value={value}
+                                     onChange={setValue}></DateRangePicker>
+                </Stepper.Step>
+                <Stepper.Step label="Son adim" description="Müşteri seçin ve fatura oluşturun">
+                    Step 2 content: Verify email
+                </Stepper.Step>
+                <Stepper.Completed>
+                    Completed, click back button to get to previous step
+                </Stepper.Completed>
+            </Stepper>
+
+            <Group position="center" mt="xl">
+                <Button variant="default" onClick={prevStep}>Back</Button>
+                <Button onClick={nextStep}>Next step</Button>
+            </Group>
+        </div>
+    )
+})
